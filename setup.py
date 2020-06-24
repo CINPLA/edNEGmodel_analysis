defc6796dd199635c468a7a1ca7e9a052de72a6f
Marte Julie <m.j.satra@fys.uio.no>
2020-06-24 14:09:53 +0200

    setup file added

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+    name="brain_tissue_module_analysis",
+    packages=find_packages(),
+    package_data={'data': ['initial_values/*.dat']},
+    version="0.1")
